599508042be90f523bed5834eaba35b039ccf721
MoNoLidThZ <admin@monolidthz.com>
2013-10-25 14:23:53 +0700

    Fix: Bottom Navbar only Load once

--- _res/ajax.php
+++ _res/ajax.php
@@ -18,6 +18,30 @@ $Browse .= "/";
 $dirs = count($Directory) - 1;
 $DIR = "../" . $safedir;
 $d = dir( $DIR );
+// Stop them accessing higher level folders
+if ( substr_count( $Browse, ".." ) > 0 )
+{
+echo MakeError("What do you think you're doing?","You don't have permission to access upper level folder!");
+//exit();
+}elseif(preg_match( "[".implode('|',$HIDDEN_DIRS)."]", $safedir ) > 0){
+echo MakeError("Access Denied","You don't have permission to access this folder!");
+}else{
+
+while (false !== ($entry = $d->read()))
+{
+if ($entry[0] == '.') continue;
+if ( in_array( $entry, $HIDDEN_FILES) ) continue;
+if ( in_array( $entry, $HIDDEN_DIRS) ) continue;
+
+// Don't list the folder if it has an index!
+if ($entry == 'index.html') exit();
+if ($entry == 'index.htm') exit();
+if ($entry == 'index.php') exit();
+if ($entry == 'index.asp') exit();
+if ( is_dir( $DIR . "/" . $entry ) ) {$dirdata['folder'][] = array( filemtime( $DIR . "/" . $entry ), $entry ); continue;}
+$dirdata[CheckFileType($entry)][] = array( filemtime( $DIR . "/" . $entry ), $entry , filesize($DIR . "/" . $entry) );
+}
+}
 switch($_GET['ajax']){
 
 case "breadcrumb"; //Get Breadcrumb Navigation
@@ -44,29 +68,7 @@ for ($i = 0; $i <= $k; $i++) {
 break;
 case "article";?>
 <?php
-// Stop them accessing higher level folders
-if ( substr_count( $Browse, ".." ) > 0 )
-{
-echo MakeError("What do you think you're doing?","You don't have permission to access upper level folder!");
-//exit();
-}elseif(preg_match( "[".implode('|',$HIDDEN_DIRS)."]", $safedir ) > 0){
-echo MakeError("Access Denied","You don't have permission to access this folder!");
-}else{
 
-while (false !== ($entry = $d->read()))
-{
-if ($entry[0] == '.') continue;
-if ( in_array( $entry, $HIDDEN_FILES) ) continue;
-if ( in_array( $entry, $HIDDEN_DIRS) ) continue;
-
-// Don't list the folder if it has an index!
-if ($entry == 'index.html') exit();
-if ($entry == 'index.htm') exit();
-if ($entry == 'index.php') exit();
-if ($entry == 'index.asp') exit();
-if ( is_dir( $DIR . "/" . $entry ) ) {$dirdata['folder'][] = array( filemtime( $DIR . "/" . $entry ), $entry ); continue;}
-$dirdata[CheckFileType($entry)][] = array( filemtime( $DIR . "/" . $entry ), $entry , filesize($DIR . "/" . $entry) );
-}
 $uid = 0; $filecount = 0;
 foreach($CONTENT_SORT as $category){
 	if(!count($dirdata[$category[0]])) continue;
@@ -80,7 +82,6 @@ foreach($CONTENT_SORT as $category){
 if($filecount < 1){
 	echo(MakeError("Empty Folder","This folder is Empty."));
 } unset($filecount);
-}
 ?>
 <?php
 break;
