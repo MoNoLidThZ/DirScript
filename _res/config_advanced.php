d8c7fee9601b4c50c8a59387545873dab130caef
MoNoLidThZ <admin@monolidthz.com>
2014-05-26 15:59:37 +0700

    Split config into 2 file
    Moved file_icons.php into config_advanced.php
    
    
    Former-commit-id: b68e2da3a9fece7de90181606bbd987f8d26420e

---
+++ _res/config_advanced.php
@@ -0,0 +1,117 @@
+<?php 
+$THUMBNAIL_FOLDER = "_thumbnails";
+//Files to be Hidden
+$HIDDEN_FILES[] = "Thumbs.db";
+$HIDDEN_FILES[] = "index.php";
+$HIDDEN_FILES[] = ".htaccess";
+$HIDDEN_FILES[] = ".htpasswd";
+$HIDDEN_FILES[] = "favicon.ico";
+//Dirs to be hidden
+$HIDDEN_DIRS[] = "_res";
+$HIDDEN_DIRS[] = $THUMBNAIL_FOLDER;
+$THUMBNAIL_WIDTH = 160;
+$THUMBNAIL_HEIGHT = 90;
+
+$name = "$!nG1_ePl[A]yErZ";	//Your name here
+$pagetitle = $name." - Index of /";
+//Use Randomly selcected header or page title?
+$showmydongers = true;
+//These are randomly selected header.
+$dong[] = "ヽ༼ຈل͜ຈ༽ﾉ your dongers seem raised ヽ༼ຈل͜ຈ༽ﾉ";
+$dong[] = "ヽ༼ຈل͜ຈ༽ﾉ raise your dongers ヽ༼ຈل͜ຈ༽ﾉ";
+$dong[] = "ヽ༼ຈل͜ຈ༽ﾉ raise your cauldrons ヽ༼ຈل͜ຈ༽ﾉ";
+$dong[] = "ノ(ಠ_ಠノ ) ʟᴏᴡᴇʀ ʏᴏᴜʀ ᴅᴏɴɢᴇʀs ノ(ಠ_ಠノ)";
+$dong[] = "ᕦ༼ຈل͜ຈ༽ᕤ flex your dongers ᕦ༼ຈل͜ຈ༽ᕤ";
+$dong[] = "ლ(ಠ_ಠლ) Screw this donger ლ(ಠ_ಠლ)";
+$dong[] = "(ง ͠° ͟ل͜ ͡°)ง ᴍᴀsᴛᴇʀ ʏᴏᴜʀ ᴅᴏɴɢᴇʀ, ᴍᴀsᴛᴇʀ ᴛʜᴇ ᴇɴᴇᴍʏ (ง ͠° ͟ل͜ ͡°)ง";
+$dong[] = "ᕙ༼ຈل͜ຈ༽ᕗ do you even lift your﻿ dongers ᕙ༼ຈل͜ຈ༽ᕗ";
+$dong[] = "༼ ºل͟º ༽ raise your mudgolems! ༼ ºل͟º ༽";
+$dong[] = "༼ ºل͟º ༽You either die a DONG, or live long enough to become the DONGER ༼ ºل͟º ༽";
+$dong[] = " ༼ ºل͟º༼ ºل͟º༼ ºل͟º༼ ºل͟º ༽ºل͟º ༽ºل͟º ༽You either die a DONG, or live long enough to become the DONGER༼ ºل͟º༼ ºل͟º༼ ºل͟º༼ ºل͟º ༽ºل͟º ༽ºل͟º ༽";
+$dong[] = "༼ つ ◕_◕ ༽つ Give DIRETIDE";
+$dong[] = "༼ᕗຈل͜ຈ༽ᕗ Drop Bows on 'em ༼ᕗຈل͜ຈ༽ᕗ";
+$dong[] = "█▄༼ຈل͜ຈ༽▄█ yeah i work out";
+$dong[] = "[̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅] Mo' money, mo' Dongers [̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅]";
+$dong[] = " ༼ ͡■ل͜ ͡■༽ OPPA DONGER STYLE ༼ ͡■ل͜ ͡■༽  ";
+$dong[] = "<ᴍᴇssᴀɢᴇ ᴅᴏɴɢᴇʀᴇᴅ>";
+//Color Class for Category Container
+/*Available colors:
+	"default" = Gray
+	"primary" = Dark Blue
+	"success" = Green
+	"info" = Light Blue
+	"warning" = Yellow
+	"danger" = Red
+*/
+$BOOTSTRAP_CLASS["folders"] = "warning";
+$BOOTSTRAP_CLASS["images"] = "success";
+$BOOTSTRAP_CLASS["video_hs"] = "primary";
+$BOOTSTRAP_CLASS["videos"] = "primary";
+$BOOTSTRAP_CLASS["audio_hs"] = "info";
+$BOOTSTRAP_CLASS["audios"] = "info";
+$BOOTSTRAP_CLASS["files"] = "default";
+//Content Sorting: What will be shown first 
+//For example: folder then html5 video then non-html5 video then html5 audio then non-html5 audio then image then file
+$CONTENT_SORT[] = array("folder","Folder");
+$CONTENT_SORT[] = array("video_h","Video");
+$CONTENT_SORT[] = array("video","Video");
+$CONTENT_SORT[] = array("audio_h","Audio");
+$CONTENT_SORT[] = array("audio","Audio");
+$CONTENT_SORT[] = array("image","Image");
+$CONTENT_SORT[] = array("file","File");
+//HTML5 MIME Types
+$MIME['mp4'] = "video/mp4";
+//User agent to redirect
+$ALLOWED_AGENTS[] = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)"; //Internet Download Manager Version 6.19
+
+//File icons, Add your extension here
+$ICON_EXT['exe'] = "ui-silk-application-xp";
+$ICON_EXT['bat'] = "ui-silk-application-xp-terminal";
+$ICON_EXT['cmd'] = "ui-silk-application-xp-terminal";
+$ICON_EXT['c'] = "ui-silk-page-white-c";
+$ICON_EXT['cpp'] = "ui-silk-page-white-cplusplus";
+$ICON_EXT['cs'] = "ui-silk-page-white-csharp";
+$ICON_EXT['cf'] = "ui-silk-page-white-coldfusion";
+$ICON_EXT['fla'] = "ui-silk-page-white-flash";
+$ICON_EXT['swf'] = "ui-silk-page-white-flash";
+$ICON_EXT['xls'] = "ui-silk-page-excel";
+$ICON_EXT['xlsx'] = "ui-silk-page-excel";
+$ICON_EXT['h'] = "ui-silk-page-white-h";
+$ICON_EXT['js'] = "ui-silk-script";
+$ICON_EXT['class'] = "ui-silk-page-white-cup";
+$ICON_EXT['java'] = "ui-silk-page-white-cup";
+$ICON_EXT['php'] = "ui-silk-page-white-php";
+$ICON_EXT['ppt'] = "ui-silk-page-white-powerpoint";
+$ICON_EXT['pptx'] = "ui-silk-page-white-powerpoint";
+$ICON_EXT['pps'] = "ui-silk-page-white-powerpoint";
+$ICON_EXT['svg'] = "ui-silk-page-white-vector";
+$ICON_EXT['svg'] = "ui-silk-page-white-visualstudio";
+$ICON_EXT['jpg'] = "ui-silk-picture";
+$ICON_EXT['gif'] = "ui-silk-picture";
+$ICON_EXT['png'] = "ui-silk-picture";
+$ICON_EXT['psd'] = "ui-silk-picture-edit";
+$ICON_EXT['psb'] = "ui-silk-picture-edit";
+$ICON_EXT['html'] = "ui-silk-page-world";
+$ICON_EXT['xhtml'] = "ui-silk-page-world";
+$ICON_EXT['htm'] = "ui-silk-page-world";
+$ICON_EXT['rar'] = "ui-silk-compress";
+$ICON_EXT['zip'] = "ui-silk-compress";
+$ICON_EXT['ace'] = "ui-silk-compress";
+$ICON_EXT['uha'] = "ui-silk-compress";
+$ICON_EXT['7z'] = "ui-silk-compress";
+$ICON_EXT['log'] = "ui-silk-page-white-text";
+$ICON_EXT['txt'] = "ui-silk-page-white-text";
+$ICON_EXT['avi'] = "ui-silk-film";
+$ICON_EXT['mpg'] = "ui-silk-film";
+$ICON_EXT['mp4'] = "ui-silk-film";
+$ICON_EXT['m4v'] = "ui-silk-film";
+$ICON_EXT['3gp'] = "ui-silk-film";
+$ICON_EXT['flv'] = "ui-silk-film";
+$ICON_EXT['webm'] = "ui-silk-film";
+$ICON_EXT['ogg'] = "ui-silk-film";
+
+//Fallback for unknown file type
+$ICON_EXT['unknown'] = "ui-silk-page";
+//Folder Icon
+$ICON_EXT['folder'] = "ui-silk-folder";?>
+?>
--- _res/config_basic.php
+++ _res/config_basic.php
@@ -1,18 +1,4 @@
 <?php 
-$THUMBNAIL_FOLDER = "_thumbnails";
-//Files to be Hidden
-$HIDDEN_FILES[] = "Thumbs.db";
-$HIDDEN_FILES[] = "index.php";
-$HIDDEN_FILES[] = ".htaccess";
-$HIDDEN_FILES[] = ".htpasswd";
-$HIDDEN_FILES[] = "favicon.ico";
-//Dirs to be hidden
-$HIDDEN_DIRS[] = "_res";
-$HIDDEN_DIRS[] = $THUMBNAIL_FOLDER;
-$HIDDEN_DIRS[] = "FATE_INTERNAL";
-$THUMBNAIL_WIDTH = 160;
-$THUMBNAIL_HEIGHT = 90;
-
 $name = "$!nG1_ePl[A]yErZ";	//Your name here
 $pagetitle = $name." - Index of /";
 //Use Randomly selcected header or page title?
@@ -35,33 +21,4 @@ $dong[] = "█▄༼ຈل͜ຈ༽▄█ yeah i work out";
 $dong[] = "[̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅] Mo' money, mo' Dongers [̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅]";
 $dong[] = " ༼ ͡■ل͜ ͡■༽ OPPA DONGER STYLE ༼ ͡■ل͜ ͡■༽  ";
 $dong[] = "<ᴍᴇssᴀɢᴇ ᴅᴏɴɢᴇʀᴇᴅ>";
-//Color Class for Category Container
-/*Available colors:
-	"default" = Gray
-	"primary" = Dark Blue
-	"success" = Green
-	"info" = Light Blue
-	"warning" = Yellow
-	"danger" = Red
-*/
-$BOOTSTRAP_CLASS["folders"] = "warning";
-$BOOTSTRAP_CLASS["images"] = "success";
-$BOOTSTRAP_CLASS["video_hs"] = "primary";
-$BOOTSTRAP_CLASS["videos"] = "primary";
-$BOOTSTRAP_CLASS["audio_hs"] = "info";
-$BOOTSTRAP_CLASS["audios"] = "info";
-$BOOTSTRAP_CLASS["files"] = "default";
-//Content Sorting: What will be shown first 
-//For example: folder then html5 video then non-html5 video then html5 audio then non-html5 audio then image then file
-$CONTENT_SORT[] = array("folder","Folder");
-$CONTENT_SORT[] = array("video_h","Video");
-$CONTENT_SORT[] = array("video","Video");
-$CONTENT_SORT[] = array("audio_h","Audio");
-$CONTENT_SORT[] = array("audio","Audio");
-$CONTENT_SORT[] = array("image","Image");
-$CONTENT_SORT[] = array("file","File");
-//HTML5 MIME Types
-$MIME['mp4'] = "video/mp4";
-//User agent to redirect
-$ALLOWED_AGENTS[] = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)"; //Internet Download Manager
 ?>
