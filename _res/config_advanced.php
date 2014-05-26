94705f47b53b3e6f6e8b986a5ab07c96de3f061d
MoNoLidThZ <admin@monolidthz.com>
2014-05-26 16:00:45 +0700

    Update config_advanced.php
    Former-commit-id: 2426d861b73566e8aa3f9dc45c92b328304d6a5a

--- _res/config_advanced.php
+++ _res/config_advanced.php
@@ -12,28 +12,6 @@ $HIDDEN_DIRS[] = $THUMBNAIL_FOLDER;
 $THUMBNAIL_WIDTH = 160;
 $THUMBNAIL_HEIGHT = 90;
 
-$name = "$!nG1_ePl[A]yErZ";	//Your name here
-$pagetitle = $name." - Index of /";
-//Use Randomly selcected header or page title?
-$showmydongers = true;
-//These are randomly selected header.
-$dong[] = "ヽ༼ຈل͜ຈ༽ﾉ your dongers seem raised ヽ༼ຈل͜ຈ༽ﾉ";
-$dong[] = "ヽ༼ຈل͜ຈ༽ﾉ raise your dongers ヽ༼ຈل͜ຈ༽ﾉ";
-$dong[] = "ヽ༼ຈل͜ຈ༽ﾉ raise your cauldrons ヽ༼ຈل͜ຈ༽ﾉ";
-$dong[] = "ノ(ಠ_ಠノ ) ʟᴏᴡᴇʀ ʏᴏᴜʀ ᴅᴏɴɢᴇʀs ノ(ಠ_ಠノ)";
-$dong[] = "ᕦ༼ຈل͜ຈ༽ᕤ flex your dongers ᕦ༼ຈل͜ຈ༽ᕤ";
-$dong[] = "ლ(ಠ_ಠლ) Screw this donger ლ(ಠ_ಠლ)";
-$dong[] = "(ง ͠° ͟ل͜ ͡°)ง ᴍᴀsᴛᴇʀ ʏᴏᴜʀ ᴅᴏɴɢᴇʀ, ᴍᴀsᴛᴇʀ ᴛʜᴇ ᴇɴᴇᴍʏ (ง ͠° ͟ل͜ ͡°)ง";
-$dong[] = "ᕙ༼ຈل͜ຈ༽ᕗ do you even lift your﻿ dongers ᕙ༼ຈل͜ຈ༽ᕗ";
-$dong[] = "༼ ºل͟º ༽ raise your mudgolems! ༼ ºل͟º ༽";
-$dong[] = "༼ ºل͟º ༽You either die a DONG, or live long enough to become the DONGER ༼ ºل͟º ༽";
-$dong[] = " ༼ ºل͟º༼ ºل͟º༼ ºل͟º༼ ºل͟º ༽ºل͟º ༽ºل͟º ༽You either die a DONG, or live long enough to become the DONGER༼ ºل͟º༼ ºل͟º༼ ºل͟º༼ ºل͟º ༽ºل͟º ༽ºل͟º ༽";
-$dong[] = "༼ つ ◕_◕ ༽つ Give DIRETIDE";
-$dong[] = "༼ᕗຈل͜ຈ༽ᕗ Drop Bows on 'em ༼ᕗຈل͜ຈ༽ᕗ";
-$dong[] = "█▄༼ຈل͜ຈ༽▄█ yeah i work out";
-$dong[] = "[̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅] Mo' money, mo' Dongers [̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅]";
-$dong[] = " ༼ ͡■ل͜ ͡■༽ OPPA DONGER STYLE ༼ ͡■ل͜ ͡■༽  ";
-$dong[] = "<ᴍᴇssᴀɢᴇ ᴅᴏɴɢᴇʀᴇᴅ>";
 //Color Class for Category Container
 /*Available colors:
 	"default" = Gray
